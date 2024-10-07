9080011d670d540e4473056509d454996a164ccf
Yutian Chen <markchenyutian@gmail.com>
2024-10-07 13:54:07 -0400

    Roughly running

--- MACVO_ROS2/MACVO.py
+++ MACVO_ROS2/MACVO.py
@@ -1,7 +1,6 @@
 import rclpy
 import torch
 import numpy as np
-import pypose as pp
 
 from rclpy.node import Node
 from sensor_msgs.msg import Image
@@ -82,11 +81,11 @@ class MACVONode(Node):
 
     def __init__(self, imageL_topic: str, imageR_topic: str, pose_topic: str, MACVO_config: str):
         super().__init__("macvo")
-        self.imageL_sub = Subscriber(self, Image, imageL_topic)
-        self.imageR_sub = Subscriber(self, Image, imageR_topic)
+        self.imageL_sub = Subscriber(self, Image, imageL_topic, queue_size=2)
+        self.imageR_sub = Subscriber(self, Image, imageR_topic, queue_size=2)
         
         self.sync_stereo = ApproximateTimeSynchronizer(
-            [self.imageL_sub, self.imageR_sub], queue_size=10, slop=0.1
+            [self.imageL_sub, self.imageR_sub], queue_size=2, slop=0.1
         )
         self.sync_stereo.registerCallback(self.receive_frame)
         
@@ -97,8 +96,11 @@ class MACVONode(Node):
         cfg, _ = load_config(Path(MACVO_config))
         self.frame_idx  = 0
         self.camera     = cfg.Camera
-        self.odometry   = MACVO.from_config(cfg, None)
-        print(self.odometry)
+        self.odometry   = MACVO.from_config(cfg)
+        
+        self.odometry.register_on_optimize_finish(self.publish_last_pose)
+        self.time  , self.prev_time  = None, None
+        self.frame , self.prev_frame = None, None
     
     @staticmethod
     def ros2_image_to_numpy(msg: Image) -> np.ndarray:
@@ -114,38 +116,13 @@ class MACVONode(Node):
         data.strides = (msg.step, dtype.itemsize * channel, dtype.itemsize)
         return data
     
-    def receive_frame(self, msg_L: Image, msg_R: Image) -> None:
-        frame        = msg_L.header.frame_id
-        imageL, time = self.ros2_image_to_numpy(msg_L), msg_L.header.stamp
-        imageR       = self.ros2_image_to_numpy(msg_R)
-        self.get_logger().info(f"Left={imageL.shape}, Right={imageR.shape}")
+    def publish_last_pose(self, system: MACVO):
+        pose = system.gmap.frames.pose[-1] if len(system.gmap.frames) == 1 else system.gmap.frames.pose[-2]
         
-        # Receive image
-        pose = pp.identity_SE3()
-        
-        meta=MetaInfo(
-            idx=self.frame_idx,
-            baseline=self.camera.bl,
-            width=self.camera.width,
-            height=self.camera.height,
-            K=torch.tensor([[self.camera.fx, 0., self.camera.cx],
-                            [0., self.camera.fy, self.camera.cy],
-                            [0., 0., 1.]])
-        )
-        frame = SourceDataFrame(
-            meta=meta,
-            imageL=torch.tensor(imageL).float() / 255.,
-            imageR=torch.tensor(imageR).float() / 255.,
-            imu=None,
-            gtFlow=None, gtDepth=None, gtPose=None, flowMask=None
-        )
-        self.odometry.run(frame)
-        
-        # Return pose
-        out_msg = PoseStamped()
-        out_msg.header = Header()
-        out_msg.header.stamp = time
-        out_msg.header.frame_id = frame
+        out_msg                 = PoseStamped()
+        out_msg.header          = Header()
+        out_msg.header.stamp    = self.time if self.prev_time is None else self.prev_time
+        out_msg.header.frame_id = self.frame if self.prev_frame is None else self.prev_frame
         
         out_msg.pose.position.x = pose[0].item()
         out_msg.pose.position.y = pose[1].item()
@@ -158,6 +135,34 @@ class MACVONode(Node):
         
         self.frame_idx += 1
         self.pose_pipe.publish(out_msg)
+    
+    def receive_frame(self, msg_L: Image, msg_R: Image) -> None:
+        self.prev_frame, self.prev_time = self.frame, self.time
+        
+        self.frame        = msg_L.header.frame_id
+        imageL, self.time = self.ros2_image_to_numpy(msg_L), msg_L.header.stamp
+        imageR       = self.ros2_image_to_numpy(msg_R)
+        self.get_logger().info(f"Receive: Left={imageL.shape}, Right={imageR.shape}, time={self.time}, frame={self.frame}")
+        
+        # Receive image
+        meta=MetaInfo(
+            idx=self.frame_idx,
+            baseline=self.camera.bl,
+            width=self.camera.width,
+            height=self.camera.height,
+            K=torch.tensor([[self.camera.fx, 0., self.camera.cx],
+                            [0., self.camera.fy, self.camera.cy],
+                            [0., 0., 1.]])
+        )
+        frame = SourceDataFrame(
+            meta=meta,
+            imageL=torch.tensor(imageL)[..., :3].float().permute(2, 0, 1).unsqueeze(0) / 255.,
+            imageR=torch.tensor(imageR)[..., :3].float().permute(2, 0, 1).unsqueeze(0) / 255.,
+            imu=None,
+            gtFlow=None, gtDepth=None, gtPose=None, flowMask=None
+        ).resize_image(scale_u=3, scale_v=2.5)
+        
+        self.odometry.run(frame)
 
 
 def main():
